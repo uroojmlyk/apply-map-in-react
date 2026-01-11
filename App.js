9a017242913fcdb1b4ba398af769ef3bff803d85
Urooj Rasheed <uroojr335@gmail.com>
2026-01-11 20:55:47 +0500

    object rendering

--- App.js
+++ App.js
@@ -1,17 +1,18 @@
 function App() {
   const users = [
-    { id: 1, name: "Ali", age: 17 },
-    { id: 2, name: "Sara", age: 22 }
+    { id: 1, name: "Ali", age: 22 },
+    { id: 2, name: "Sara", age: 19 }
   ];
 
   return (
     <div>
-      <h2>User Status</h2>
+      <h2>Users</h2>
 
       {users.map((user) => (
-        <p key={user.id}>
-          {user.name} - {user.age >= 18 ? "Adult" : "Minor"}
-        </p>
+        <div key={user.id}>
+          <p>Name: {user.name}</p>
+          <p>Age: {user.age}</p>
+        </div>
       ))}
     </div>
   );
